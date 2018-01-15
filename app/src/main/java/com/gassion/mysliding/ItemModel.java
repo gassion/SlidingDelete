98597ba703034ba7eddf93ced73064544a6a1335
“gassion <5214086@qq.com>
2018-01-15 18:49:04 +0800

    添加实例代码

---
+++ app/src/main/java/com/gassion/mysliding/ItemModel.java
@@ -0,0 +1,9 @@
+package com.gassion.mysliding;
+
+import java.io.Serializable;
+
+public class ItemModel implements Serializable {
+    public int imgRes;
+    public String title;
+    public String desc;
+}
--- app/src/main/java/com/gassion/mysliding/MainActivity.java
+++ app/src/main/java/com/gassion/mysliding/MainActivity.java
@@ -3,11 +3,37 @@ package com.gassion.mysliding;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.gassion.slidingdeletelibrary.SwipeListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+
+    private SwipeListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mListView = (SwipeListView) findViewById(R.id.swipeListView);
+        List<ItemModel> items = new ArrayList<ItemModel>();
+        ItemModel item = null;
+        for (int i = 0; i < 3; i++) {
+            item = new ItemModel();
+            item.imgRes = R.mipmap.ic_launcher;
+            item.title = "swipeListView_" + i;
+            item.desc = "你说好不好用呢 还有什么改进可以联系我";
+            items.add(item);
+        }
+        SwipeAdapter swipeAdapter = new SwipeAdapter(this);
+        if (items != null && !items.isEmpty()) {
+            swipeAdapter.setListData(items);
+        }
+        mListView.setAdapter(swipeAdapter);
     }
 }
