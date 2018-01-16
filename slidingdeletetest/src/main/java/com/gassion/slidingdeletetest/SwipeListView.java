10986ec7fe2b78b2878809503722e561c60c930e
“gassion <5214086@qq.com>
2018-01-16 14:48:59 +0800

    重新添加库文件

---
+++ slidingdeletetest/src/main/java/com/gassion/slidingdeletetest/SwipeListView.java
@@ -0,0 +1,60 @@
+package com.gassion.slidingdeletetest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListView;
+
+public class SwipeListView extends ListView {
+    public static SwipeView mSwipeView;
+    private int mPosition;
+
+    public SwipeListView(Context context) {
+        super(context);
+    }
+
+    public SwipeListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void shrinkItem(int position) {
+        View item = getChildAt(position);
+
+        if (item != null) {
+            try {
+                ((SwipeView) item).shrink();
+            } catch (ClassCastException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                mSwipeView = null;
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                int position = pointToPosition(x, y);
+                if (position != INVALID_POSITION) {
+                    int firstPos = getFirstVisiblePosition();
+                    mSwipeView = (SwipeView) getChildAt(position - firstPos);
+                }
+            }
+            default:
+                break;
+        }
+
+        if (mSwipeView != null) {
+            mSwipeView.onSlideTouchEvent(event);
+        }
+        return super.onTouchEvent(event);
+    }
+
+}
